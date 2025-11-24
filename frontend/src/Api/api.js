f6548e83de35d4217ff23d70d9b90292d56750a5
kapish kumar swami <kapishk2003@gmail.com>
2025-11-24 11:22:17 +0530

    Update api.js

--- frontend/src/Api/api.js
+++ frontend/src/Api/api.js
@@ -1,8 +1,8 @@
-
 import axios from 'axios';
-const LOCALHOST='http://localhost:5054'
 
-export const API_BASE_URL = LOCALHOST
+const LIVE_BACKEND_URL = "https://projectmanagement-5.onrender.com";
+
+export const API_BASE_URL = LIVE_BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -11,7 +11,6 @@ const api = axios.create({
 const token = localStorage.getItem('jwt');
 
 api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
 api.defaults.headers.post['Content-Type'] = 'application/json';
 
 export default api;
